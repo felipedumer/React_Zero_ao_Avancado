3ccc42ae8aa438ec30fc69432d0365ee3197b797
Felipe <felipedumer@gmail.com>
2020-06-22 20:15:21 -0300

    Styled components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react';
-import "./style.css";
-import Routes from "./Routes";
+import {BemVindo, Container, Head, Titulo} from './styles';
 
 class App extends Component {
 
     render() {
         return (
-            <Routes/>
+            <Container>
+                <Head>
+                    <Titulo>Projeto styled</Titulo>
+                </Head>
+
+                <BemVindo cor={"00FF00"} tamanho={15}>
+                    Bem vindo ao sistema
+                </BemVindo>
+            </Container>
         );
     }
 }
 
 export default App;
+
+/*
+            <div className={"Container"}>
+                <header className={"header"}>
+                   <a className={"titulo"}>Projeto Styled</a>
+                </header>
+
+                <h1>Bem-vindo!</h1>
+            </div>
+            */
