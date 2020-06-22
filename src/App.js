bef02ba7526240acba4c9aea9f28ab6afb937545
Felipe <felipedumer@gmail.com>
2020-06-22 01:25:53 -0300

    Projeto ToDo List e trabalhando com funções recebidas

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React, {Component} from 'react';
+import TodoList from './components/TodoList';
 import "./estilo.css";
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            nutri: []
-        };
-    }
-
-    componentDidMount () {
-        let url = "https://sujeitoprogramador.com/rn-api/?api=posts"
-        fetch(url)
-            .then((response) => response.json())
-            .then((json) => {
-                let state = this.state;
-                state.nutri = json;
-                this.setState(state);
-            });
-    }
-
     render() {
         return (
-            <div className={"container"}>
-                <header>
-                    <strong>Nutri</strong>
-                </header>
-                {this.state.nutri.map((item) => {
-                    return(
-                        <article key={item.id} className={"post"}>
-                            <strong className={"titulo"}>{item.titulo}</strong>
-                            <img className={"capa"} src={item.capa}/>
-                            <p className={"subtitulo"}>{item.subtitulo}</p>
-                            <a className={"botao"} href={"#"}>Acessar</a>
-                        </article>
-                    )
-                })}
+            <div>
+                <h1>Lista de Tarefas</h1>
+                <TodoList/>
             </div>
         );
     }
