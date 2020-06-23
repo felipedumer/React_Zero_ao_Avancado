0a867c62631c4d75e785beb96fac62587eff0543
Felipe <felipedumer@gmail.com>
2020-06-23 19:48:19 -0300

    Criando usuarios e associando com ID, firebase

--- src/App.js
+++ src/App.js
@@ -8,48 +8,59 @@ export default class App extends Component {
 
         this.state = {
             email: '',
-            senha: ''
+            senha: '',
+            nome: ''
         };
 
-        this.logar = this.logar.bind(this);
-        // deslogar
-        //firebase.auth().signOut();
+        this.sair = this.sair.bind(this);
+        this.cadastrar = this.cadastrar.bind(this);
+
+        firebase.auth().signOut();
 
         // verificar se houve alteração no stats
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
-                alert('usuario logado com sucesso)');
+                firebase.database().ref('usuarios').child(user.uid).set({
+                    nome: this.state.nome
+                })
             }
         });
     }
 
-    logar(e) {
+    cadastrar(e) {
 
-        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.senha)
+        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.senha)
             .catch((err) => {
-                if(err.code === 'auth/wrong-password') {
-                    alert('Senha incorreta')
-                }
-                else {
                     alert('Codigo' + err.code);
                 }
-            })
-        e.preventDefault()
+            ).then(() => {
+                this.setState({nome: '', email: '', senha: ''});
+        });
+        e.preventDefault();
+    }
+
+    sair (e) {
+        firebase.auth().signOut();
+        e.preventDefault();
     }
 
     render() {
         return (
             <div>
-                <h1>Entrar</h1>
-                <form onSubmit={this.logar}>
-                    <lavel>Email:</lavel><br/>
+                <h1>Novo Usuario</h1>
+                <form onSubmit={(e) => this.cadastrar(e)}>
+                    <br/><label>Nome:</label><br/>
+                    <input type={"text"} value={this.state.nome}
+                           onChange={(e) => this.setState({nome: e.target.value})}/>
+                    <br/><label>Email:</label><br/>
                     <input type={"text"} value={this.state.email}
                            onChange={(e) => this.setState({email: e.target.value})}/>
                     <br/><label>Senha:</label><br/>
                     <input type={"text"} value={this.state.senha}
                            onChange={(e) => this.setState({senha: e.target.value})}/>
-                    <br/><button type={"submit"}>Entrar</button>
+                    <br/><button type={"submit"}>Cadastrar</button>
                 </form>
+                <br/><button onClick={this.sair}>Sair</button>
             </div>
         );
     }
