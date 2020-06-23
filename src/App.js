62654912949068af4585a0e4dc2577b2ab1f93c3
Felipe <felipedumer@gmail.com>
2020-06-23 20:18:44 -0300

    Autenticações e renderização condicional

--- src/App.js
+++ src/App.js
@@ -9,58 +9,82 @@ export default class App extends Component {
         this.state = {
             email: '',
             senha: '',
-            nome: ''
+            user: null
         };
 
-        this.sair = this.sair.bind(this);
         this.cadastrar = this.cadastrar.bind(this);
-
-        firebase.auth().signOut();
-
-        // verificar se houve alteração no stats
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                firebase.database().ref('usuarios').child(user.uid).set({
-                    nome: this.state.nome
-                })
-            }
-        });
+        this.logar = this.logar.bind(this);
+        this.ver = this.ver.bind(this);
+        this.sair = this.sair.bind(this);
     }
 
-    cadastrar(e) {
+    componentDidMount() {
+        this.ver();
+    }
 
+    ver() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({user: user});
+            }
+        })
+    }
+
+    cadastrar() {
         firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.senha)
             .catch((err) => {
                     alert('Codigo' + err.code);
                 }
-            ).then(() => {
-                this.setState({nome: '', email: '', senha: ''});
-        });
-        e.preventDefault();
+            );
     }
 
-    sair (e) {
-        firebase.auth().signOut();
-        e.preventDefault();
+    sair(){
+        firebase.auth().signOut()
+            .then(()=>{
+                this.setState({user: null, email: '', senha:''});
+            })
+        ;
+    }
+
+    logar() {
+        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.senha)
+            .then((e) => {
+                this.setState({email: ''});
+            })
+            .catch((error) => {
+                alert('Error: ' + error);
+            })
+
     }
 
     render() {
         return (
             <div>
-                <h1>Novo Usuario</h1>
-                <form onSubmit={(e) => this.cadastrar(e)}>
-                    <br/><label>Nome:</label><br/>
-                    <input type={"text"} value={this.state.nome}
-                           onChange={(e) => this.setState({nome: e.target.value})}/>
-                    <br/><label>Email:</label><br/>
-                    <input type={"text"} value={this.state.email}
-                           onChange={(e) => this.setState({email: e.target.value})}/>
-                    <br/><label>Senha:</label><br/>
-                    <input type={"text"} value={this.state.senha}
-                           onChange={(e) => this.setState({senha: e.target.value})}/>
-                    <br/><button type={"submit"}>Cadastrar</button>
-                </form>
-                <br/><button onClick={this.sair}>Sair</button>
+                {this.state.user ?
+                    <div>
+                        <p>Painel Admin</p>
+                        <p>Bem-vindo</p>
+                        <p>{this.state.user.email}</p>
+                        <p>{this.state.user.uid}</p>
+                        <button onClick={this.sair}>Sair</button>
+                    </div>
+                    :
+                    <div>
+                        <h1>Seja bem-vindo</h1>
+                        <br/>
+                        <label>Email:</label><br/>
+                        <input type={"email"} value={this.state.email}
+                               onChange={(e) => this.setState({email: e.target.value})}/>
+                        <br/>
+                        <label>Senha:</label>
+                        <br/>
+                        <input type={"text"} value={this.state.senha}
+                               onChange={(e) => this.setState({senha: e.target.value})}/>
+                        <br/>
+                        <button onClick={this.cadastrar}>Cadastrar</button>
+                        <button onClick={this.logar}>Logar</button>
+                    </div>
+                }
             </div>
         );
     }
