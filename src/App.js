ae8862ef0dccd6aa672f3bcb086d7b5bd034eab3
Felipe <felipedumer@gmail.com>
2020-06-23 18:31:22 -0300

    Firebase, puxando informações.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
 import React, {Component} from 'react';
-import Routes from "./Routes";
+import * as firebase from 'firebase';
 
-class App extends Component {
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            token: 'Carregando...',
+            nome: '',
+            idade: ''
+        };
+
+        let firebaseConfig = {
+            apiKey: "AIzaSyC1kaZOBjaO4wbCsD3p5lXE2eecREwX-eI",
+            authDomain: "iniciandofirebase-12eb9.firebaseapp.com",
+            databaseURL: "https://iniciandofirebase-12eb9.firebaseio.com",
+            projectId: "iniciandofirebase-12eb9",
+            storageBucket: "iniciandofirebase-12eb9.appspot.com",
+            messagingSenderId: "23435037090",
+            appId: "1:23435037090:web:60794c791d5f52a7fdcafb",
+            measurementId: "G-K182T3L4LC"
+        };
+        // Initialize Firebase
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+        firebase.analytics();
+
+        /* Olheiro
+        firebase.database().ref('token').on('value', (snapshot) => {
+            let state = this.state;
+            state.token = snapshot.val();
+            this.setState(state);
+        });*/
+
+        // consulta única vez
+        firebase.database().ref('token').once('value').then((snapshot) => {
+            let state = this.state;
+            state.token = snapshot.val();
+            this.setState(state);
+        });
+
+        firebase.database().ref('usuarios').child(1).on('value', (snapshot) => {
+            let state = this.state;
+            state.nome = snapshot.val().nome;
+            state.idade = snapshot.val().idade;
+            this.setState(state);
+        });
+
+    }
 
     render() {
+        const {token, nome, idade} = this.state;
+
         return (
-            <Routes/>
+            <div>
+                <h1>Token: {token}</h1>
+                <h1>Nome: {nome}</h1>
+                <h1>Idade: {idade}</h1>
+            </div>
         );
     }
 }
-
-export default App;
