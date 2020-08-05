e241a0a47cd8151543cc58eefc27d1da663bdc1b
Specm <felipedumer@gmail.com>
2020-08-04 22:59:10 -0300

    Projeto repositorio parte 6 listando e deletando repositorios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,13 @@
 import React, {useState, useCallback} from 'react';
-import {Container, Formulario, SubmitButton} from './styles';
-import {FaGithub, FaPlus, FaSpinner} from 'react-icons/fa';
+import {Container, Formulario, SubmitButton, List, DeleteButton} from './styles';
+import {FaGithub, FaPlus, FaSpinner, FaBars, FaTrash} from 'react-icons/fa';
 
 import api from '../../services/api';
 
 export default function Main() {
 
     const [newRepo, setNewRepo] = useState('');
-    const [repositorios, setRepositorios] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
     const [loading, setLoading] = useState(false);
 
     const handleSubmit = useCallback((e) => {
@@ -40,6 +40,11 @@ export default function Main() {
         setNewRepo(e.target.value);
     }
 
+    const handleDelete = useCallback((repo) => {
+        const find = repositorios.filter(r => r.name !== repo)
+        setRepositorios(find);
+    }, [repositorios]);
+
     return (
         <Container>
 
@@ -65,6 +70,24 @@ export default function Main() {
                     </SubmitButton>
 
             </Formulario>
+
+            <List>
+                {repositorios.map(repo => (
+                    <li key={repo.name}>
+
+                        <span>
+                            <DeleteButton onClick={() => handleDelete(repo.name)}>
+                                <FaTrash size={14}/>
+                            </DeleteButton>
+                            {repo.name}
+                        </span>
+
+                        <a href={""}>
+                            <FaBars size={"20"}/>
+                        </a>
+                    </li>
+                ))}
+            </List>
 
         </Container>
     )
