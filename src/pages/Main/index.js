d4ce5cf11cf3eaaffc4d166eb35a8d0f813cfbfe
Specm <felipedumer@gmail.com>
2020-08-04 21:44:32 -0300

    Projeto repositorio parte 3 estiligando Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import {Title} from './styles';
+import {Container, Formulario, SubmitButton} from './styles';
+import {FaGithub, FaPlus} from 'react-icons/fa';
 
 export default function Main() {
     return (
-        <Title>Main</Title>
+        <Container>
+
+            <h1>
+                <FaGithub size={25}/>
+                Meus repositórios
+            </h1>
+
+            <Formulario onSubmit={() => {}}>
+                <input type={"text"} placeholder={"Adicionar Repositorios"}/>
+
+                <SubmitButton>
+                    <FaPlus> color="#FFF" size={14}</FaPlus>
+                </SubmitButton>
+
+            </Formulario>
+
+        </Container>
     )
 }
