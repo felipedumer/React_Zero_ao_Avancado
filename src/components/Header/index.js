ebf805243b4716373d2b1d580c95829ae0b3dd48
Specm <felipedumer@gmail.com>
2020-08-05 23:11:14 -0300

    Projeto redux parte 2 estilizando header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import logo from '../../assets/logo.png';
+import './styles.css';
 
 export default function Header(){
     return (
-        <header>
-            <h1>Header</h1>
+        <header className={"container"}>
+            <Link to={"/"}>
+                <img className={"logo"} src={logo} alt={"Logo"}/>
+            </Link>
+            <Link className={"reservas"} to={"/reservas"}>
+                <div>
+                    <strong>Minhas reservas</strong>
+                    <span>0 Reservas</span>
+                </div>
+            </Link>
         </header>
     )
 }
