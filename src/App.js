b7c598342d32e3a0e46ff5f56bb48dd7434df5b9
Specm <felipedumer@gmail.com>
2020-08-06 00:55:30 -0300

    Projeto redux parte 6 configurando redux

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import Routes from "./routes";
 import Header from "./components/Header";
+import {Provider} from 'react-redux';
+
+import store from "./store";
 
 function App() {
     return (
+        <Provider store={store}>
         <BrowserRouter>
             <Header/>
             <Routes/>
         </BrowserRouter>
+        </Provider>
     );
 }
 
