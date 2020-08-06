897c0df0f7c15a36ca25632b21cae0e35948d5ba
Specm <felipedumer@gmail.com>
2020-08-06 17:07:07 -0300

    Projeto redux parte 8 Adicionando reservas

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import {MdDelete} from 'react-icons/md'
 import './styles.css';
 
 export default function Reservas(){
+
+    const reserves = useSelector(state => state.reserve);
+
     return (
         <div>
-            <h1 className={"title"}>Vc solicitou 1 reserv</h1>
+            <h1 className={"title"}>Vc solicitou {reserves.length} reservas</h1>
 
-            <div className={"reservas"}>
-                <img
-                src={"https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"}
-                alt={"maceio"}/>
-                <strong>Viagem Maceio 7 dias</strong>
-                <span>Quantidade: 2</span>
-                <button
-                type={"button"}
-                onClick={() => {}}
-                >
-                <MdDelete size={19} color={"#FFF"}/>
-                </button>
-            </div>
+            {reserves.map(reserve => (
+                <div className={"reservas"} key={reserve.id}>
+                    <img
+                        src={reserve.image}
+                        alt={reserve.title}/>
+                    <strong>{reserve.title}</strong>
+                    <span>Quantidade: 2</span>
+                    <button
+                        type={"button"}
+                        onClick={() => {}}
+                    >
+                        <MdDelete size={19} color={"#FFF"}/>
+                    </button>
+                </div>
+            ))}
+
 
             <footer>
                 <button type={"button"}>Solicitar Reservas</button>
